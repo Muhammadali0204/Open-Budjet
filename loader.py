62a828daa614d4aab0aee90a5ffb7da43a28c7ea
Muhammadali0204 <mamadjonovmuhammadali7@gmail.com>
2023-03-27 05:52:59 +0500

    loader

--- loader.py
+++ loader.py
@@ -7,13 +7,6 @@ bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 db = sqlite.Database("data/Users.db")
-ovoz_link = db.select_user_by_id(1)[3]
-baza_link = db.select_user_by_id(2)[3]
-obunachiga_pul = db.select_user_by_id(3)[2]
-taklifga_pul = db.select_user_by_id(4)[2]
-vaqt = {}
-karta = {}
-tolangan_malumot = {}
 try:
     db.create_table_users()
 except:
@@ -23,3 +16,11 @@ try:
     db.create_table_taklifqilganlar()
 except:
     pass
+ovoz_link = db.select_user_by_id(1)[3]
+baza_link = db.select_user_by_id(2)[3]
+obunachiga_pul = db.select_user_by_id(3)[2]
+taklifga_pul = db.select_user_by_id(4)[2]
+vaqt = {}
+karta = {}
+tolangan_malumot = {}
+
